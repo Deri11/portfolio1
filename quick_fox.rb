cbc08c670cc9a7a9fb351668c34d60cb890034b6
Timothy Ingram <tim.ringram@gmail.com>
2016-04-20 13:15:31 -0400

    Adds hw file

---
+++ quick_fox.rb
@@ -0,0 +1,38 @@
+class Fox
+  def jumped_over(animal, pet)
+  end
+end
+
+lazy_dog = "Moo"
+daisy_log = "Freddie"
+quick_fox = Fox.new
+
+quick_fox.jumped_over(lazy_dog, daisy_log)
+
+
+# class Quadrilateral
+# end
+#
+# class Rectangle < Quadrilateral
+# end
+#
+# class Trapezoid < Quadrilateral
+# end
+#
+# class Square < Rectangle
+# end
+#
+# class Rhombus < Trapezoid
+# end
+#
+# def test
+#   squa = Square.new
+#   puts squa.is_a? Rectangle
+#   puts squa.is_a? Quadrilateral
+#
+#   rect = Rectangle.new
+#   puts rect.is_a? Quadrilateral
+#   puts not(rect.is_a? Trapezoid)
+# end
+#
+# test
